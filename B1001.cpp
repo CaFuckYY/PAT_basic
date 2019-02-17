8ea050e2c037dbf6c1ec5b0c80bfcc57d6175567
caimingwei <caimingwei@caimingwei.local>
2019-02-17 14:14:13 +0800

    msg

---
+++ B1001.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+int main(){
+	int count=0;
+	int n;
+	scanf("%d",&n);
+	while(n!=1){
+		if(n%2==0){
+			n=n/2;
+			count++;
+		}else{
+			n=(3*n+1)/2;
+			count++;
+		}
+	}
+	printf("%d\n",count);
+	return 0;
+}
