323d5ceb4f03f95cdbc70961e15073821904b909
cmw <568258340@qq.com>
2019-03-07 11:15:09 +0800

    msg

--- B1025.cpp
+++ B1025.cpp
@@ -1,51 +1,45 @@
 #include<cstdio>
 #include<algorithm>
 using namespace std;
-struct node{
-	int address;
-	int data;
-	int next;
-	int order;
-}nodes[100010];
-bool cmp(node a,node b){
+struct Node{
+	int add,data,next,order;
+}node[100010];
+bool cmp(Node a,Node b){
 	return a.order<b.order;
 }
 int main(){
-	int add,n,k;
-	scanf("%d %d %d",&add,&n,&k);
-	for(int i=0;i<100010;i++) nodes[i].order=100010;
+	int begin,n,k;
+	scanf("%d %d %d",&begin,&n,&k);
 	for(int i=0;i<n;i++){
 		int address;
 		scanf("%d",&address);
-		nodes[address].address=address;
-		scanf("%d %d",&nodes[address].data,&nodes[address].next);
+		node[address].add=address;
+		scanf("%d %d",&node[address].data,&node[address].next);
 	}
-	int len=0;
-	int p=add;
+	for(int i=0;i<100010;i++) node[i].order=100010;
+	int count=0;
+	int p=begin;
 	while(p!=-1){
-		nodes[p].order=len++;
-		p=nodes[p].next;
+		//printf("%d %d %d\n",node[p].add,node[p].data,node[p].next);
+		node[p].order=count++;
+		p=node[p].next;
 	}
-	sort(nodes,nodes+100010,cmp);
-	for(int i=0;i<len/k;i++){
+	sort(node,node+100010,cmp);
+	n=count;
+	for(int i=0;i<n/k;i++){
 		for(int j=(i+1)*k-1;j>i*k;j--){
-			printf("%05d %d %05d\n",nodes[j].address,nodes[j].data,nodes[j-1].address);
+			printf("%05d %d %05d\n",node[j].add,node[j].data,node[j-1].add);
 		}
-		printf("%05d %d ",nodes[i*k].address,nodes[i*k].data);
-		if(i<n/k-1){
-			printf("%05d\n",nodes[(i+2)*k-1].address);
-		}else{
-			if(n%k==0){
-				printf("-1\n");
-			}else{
-				printf("%05d\n",nodes[(i+1)*k].address);
-				for(int o=len/k*k;o<len;o++){
-					printf("%05d %d ",nodes[o].address,nodes[o].data);
-					if(o<len-1){
-						printf("%05d\n",nodes[o+1].address);
-					}else{
-						printf("-1\n");
-					}
+		printf("%05d %d ",node[i*k].add,node[i*k].data);
+		if(i<n/k-1) printf("%05d\n",node[(i+2)*k-1].add);
+		else{
+			if(n%k==0) printf("-1\n");
+			else{
+				printf("%05d\n",node[(i+1)*k].add);
+				for(int i=n/k*k;i<n;i++){
+					printf("%05d %d ",node[i].add,node[i].data);
+					if(i==n-1) printf("-1\n");
+					else printf("%05d\n",node[i+1].add);
 				}
 			}
 		}
